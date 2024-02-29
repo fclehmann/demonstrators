37746baf35d09dfec2bb423b03487178845f1348
Christoph Lehmann <christoph.lehmann@tu-dresden.de>
2024-02-29 13:58:02 +0100

    added first running prototype with freehand mode. classification calc not yet working.

---
+++ bs4dash/test_minimal/classifier_calculations.R
@@ -0,0 +1,118 @@
+# classifier_calculations.R
+
+generateData <- function(group1_params, group2_params) {
+  # Generate bivariate normal data for Group 1
+  data1 <- mvrnorm(n = group1_params$n,
+                   mu = c(group1_params$mean1, group1_params$mean2),
+                   Sigma = matrix(c(group1_params$sd1^2, group1_params$cor * group1_params$sd1 * group1_params$sd2,
+                                    group1_params$cor * group1_params$sd1 * group1_params$sd2, group1_params$sd2^2), nrow = 2),
+                   empirical = FALSE)
+  
+  # Generate bivariate normal data for Group 2
+  data2 <- mvrnorm(n = group2_params$n,
+                   mu = c(group2_params$mean1, group2_params$mean2),
+                   Sigma = matrix(c(group2_params$sd1^2, group2_params$cor * group2_params$sd1 * group2_params$sd2,
+                                    group2_params$cor * group2_params$sd1 * group2_params$sd2, group2_params$sd2^2), nrow = 2),
+                   empirical = FALSE)
+  
+  # Combine the data with group assignments
+  group1 <- data.frame(Variable1 = data1[, 1], Variable2 = data1[, 2], Group = as.factor("Group1"))
+  group2 <- data.frame(Variable1 = data2[, 1], Variable2 = data2[, 2], Group = as.factor("Group2"))
+  
+  return(rbind(group1, group2))
+}
+
+# Function to calculate decision boundary parameters from two points
+calculate_linear_boundary <- function(input_coordinates) {
+  info(logger, paste('entering calculate_linear_boundary, input vector length: ', length(input_coordinates$x)))
+  info(logger, paste('vector x: ', input_coordinates$x, '   ', 'vector y: ', input_coordinates$y))
+  # Ensure there are two clicked points
+  if (length(input_coordinates$x) != 2) {
+    return(NULL)
+  }
+  
+  # Extract the coordinates of the two clicked points
+  x1 <- input_coordinates$x[1]
+  y1 <- input_coordinates$y[1]
+  x2 <- input_coordinates$x[2]
+  y2 <- input_coordinates$y[2]
+  
+  # Calculate slope and intercept
+  slope <- (y2 - y1) / (x2 - x1)
+  intercept <- y1 - slope * x1
+  
+  # Return the parameters as a list
+  return(list(slope = slope, intercept = intercept))
+}
+
+is_above_linear <- function(intercept, slope, compare_data){
+  info(logger, 'entering is_above_linear...')
+  info(logger, paste('linear params: ', intercept, slope))
+  y_predicted <- intercept + slope*compare_data$Variable1
+  above <- ifelse(compare_data$Variable2 > y_predicted, TRUE, FALSE)
+  return(above)
+}
+
+is_above_freehandline <- function(x, y, compare_data, df_spline){
+  # fitting spline
+  smoothed <- smooth.spline(x, y, df = df_spline)
+  
+  # relate spline values for dedicated x-values to data y data points
+  y_predicted <- predict(smoothed, compare_data$Variable1)$y
+  above <- ifelse(compare_data$Variable2 > y_predicted, TRUE, FALSE)
+  return(above)
+}
+
+calculate_classification_results <- function(referenceData, aboveData){
+  tmp_df <- data.frame(Group = referenceData,
+                       AboveBoundary = as.factor(aboveData)
+  )
+  tmp_df %<>% mutate(classification_group1_below = as.factor(ifelse(AboveBoundary==FALSE, 'Group1', 'Group2')), 
+                     classification_group1_above = as.factor(ifelse(AboveBoundary==TRUE, 'Group1', 'Group2')))
+  
+  # Calculate F1 score for classification_group1_below
+  tmp_below <- confusionMatrix(
+    tmp_df$Group,
+    tmp_df$classification_group1_below,
+    positive = "Group1"
+  )
+  print(tmp_below$byClass[7])
+  print(tmp_above$byClass[7])
+  # Calculate F1 score for classification_group1_above
+  tmp_above <- confusionMatrix(
+    tmp_df$Group,
+    tmp_df$classification_group1_above,
+    positive = "Group1"
+  )
+
+  # Check for perfect separation
+  perfect_separation <- is.nan(tmp_below$byClass[7]) || is.nan(tmp_above$byClass[7])
+  print(colnames(tmp_df))
+  # Determine which classification to choose based on F1 scores
+  if (perfect_separation) {
+    # Handle perfect separation case
+    # For example, choose the classification with the most observations
+    if (sum(tmp_df$Group == "Group1") > sum(tmp_df$Group == "Group2")) {
+      tmp_df %<>%
+        select(-classification_group1_above) %>%
+        rename(classification = classification_group1_below)
+    } else {
+      tmp_df %<>%
+        select(-classification_group1_below) %>%
+        rename(classification = classification_group1_above)
+    }
+  } else {
+    # Regular case: choose based on F1 scores
+    if (tmp_below$byClass[7] >= tmp_above$byClass[7]) {
+      tmp_df %<>%
+        select(-classification_group1_above) %>%
+        rename(classification = classification_group1_below)
+    } else {
+      tmp_df %<>%
+        select(-classification_group1_below) %>%
+        rename(classification = classification_group1_above)
+    }
+  }
+  # print(tmp_df)
+  return(tmp_df)
+}
--- bs4dash/test_minimal/freehand_prototype.R
+++ bs4dash/test_minimal/freehand_prototype.R
@@ -1,54 +1,170 @@
 library(shiny)
+library(bs4Dash)
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+library(caret)
+library(MASS)
+library(log4r)
 
-# Zeichenlogik: 
-# einheitlich: 2 klicks begrenzen die Zeichenaktivität
-# abweichung: 
-#   - gespeicherte koordinaten
-#   - gezeichnetes Objekt (Punkte vs. Linie)
+# Define logger configuration
+logger <- logger()
 
-# Load the dataset
-data(mtcars)
+source('classifier_calculations.R')
+source('drawing_functions.R')
 
-
-ui <- fluidPage(
-  h4("Click on plot to start draw_freehanding, click again to pause"),
-  actionButton(inputId = "reset", label = "Reset"),
-  radioButtons(inputId = "drawType", 
-               label = "Drawing Mode:",
-               choices = list("Draw Line" = "line", "Draw Freehand" = "freehand"),
-               selected = "line"),
-  checkboxInput(inputId = "use_rbaseplot", label = "Use R baseplot", value = FALSE),
-  fluidRow(
-    column(width = 6, plotOutput("plot", width = "100%", height = "500px",
-                                 hover=hoverOpts(id = "plot_hover", delay = 180, delayType = "throttle", clip = TRUE, nullOutside = TRUE),
-                                 click="plot_click"))
+############ ui stuff ######################
+ui = { 
+  dashboardPage(
+  title = "Classifier Demo",
+  header = dashboardHeader(
+    title = dashboardBrand(
+      title = "Data Settings",
+      color = "primary",
+      href = "https://scads.ai/",
+      image = "https://scads.ai/wp-content/themes/scads/assets/images/scads_logo.svg"
+    )
+  ),
+  sidebar = dashboardSidebar(
+    minified = FALSE,
+    # data area
+    tabsetPanel(
+      tabPanel(title = "Group 1", 
+               sliderInput("n1", "Number of Observations:", value = 5, min = 10, max = 200, step = 5),
+               fluidRow(
+                 column(6, checkboxInput("equal_mean_1", label = "Equalize Mean", value = TRUE)),
+                 column(6, checkboxInput("equal_sd_1", label = "Equalize SD", value = TRUE))
+               ),
+               sliderInput("mean1_1", "Mean of Variable 1:", value = -2, min = -10, max = 10),
+               conditionalPanel(
+                 condition = "input.equal_mean_1 == false",
+                 sliderInput("mean2_1", "Mean of Variable 2:", value = -2, min = -10, max = 10)
+               ),
+               sliderInput("sd1_1", "Standard Deviation of Variable 1:", value = 1, min = 0.1, max = 5, step = 0.1),
+               conditionalPanel(
+                 condition = "input.equal_sd_1 == false",
+                 sliderInput("sd2_1", "Standard Deviation of Variable 2:", value = 1, min = 0.1, max = 5, step = 0.1)
+               ),
+               sliderInput("cor_1", "Correlation coefficient:", min = -1, max = 1, value = 0, step = 0.1)
+      ),
+      tabPanel(title = "Group 2",
+               sliderInput("n2", "Number of Observations:", value = 5, min = 10, max = 200, step = 5),
+               fluidRow(
+                 column(6, checkboxInput("equal_mean_2", label = "Equalize Mean", value = TRUE)),
+                 column(6, checkboxInput("equal_sd_2", label = "Equalize SD", value = TRUE))
+               ),
+               sliderInput("mean1_2", "Mean of Variable 1:", value = 0, min = -10, max = 10),
+               conditionalPanel(
+                 condition = "input.equal_mean_2 == false",
+                 sliderInput("mean2_2", "Mean of Variable 2:", value = 0, min = -10, max = 10)
+               ),
+               sliderInput("sd1_2", "Standard Deviation of Variable 1:", value = 1, min = 0.1, max = 5, step = 0.1),
+               conditionalPanel(
+                 condition = "input.equal_sd_2 == false",
+                 sliderInput("sd2_2", "Standard Deviation of Variable 2:", value = 1, min = 0.1, max = 5, step = 0.1)
+               ),
+               sliderInput("cor_2", "Correlation coefficient:", min = -1, max = 1, value = 0, step = 0.1)
+      )
+    ), 
+    br(),
+    checkboxInput(inputId = "use_rbaseplot", label = "Use R baseplot", value = TRUE)
+  ),
+  # core content 
+  body = dashboardBody(
+    box(title = 'Task description',
+        width = 12,
+        fluidRow(
+          column(width = 2, "There are given two groups of people where something was observed. Please try to seperate these two groups as good as possible by drawing a straight line into the scatterplot.",),
+          column(width = 2, 
+                 align = "center", 
+                 radioButtons(inputId = "drawType", 
+                              label = "Drawing Mode:",
+                              choices = list("Draw Line" = "line", "Draw Freehand" = "freehand"),
+                              selected = "line"), 
+                 br(),
+                 actionButton(inputId = "reset", label = "Reset")
+          ),
+          column(width = 2, align = "right", tableOutput("AboveTable")), 
+          column(width = 3, align = "center", verbatimTextOutput("classificationRule")),
+          column(width = 3, align = "center", tableOutput("classificationTable"))
+        )
+    ),
+    box(
+      #title = "Scatterplot",
+      width = 12,
+      plotOutput("plot", 
+                 height = "600px", 
+                 click = "plot_click", 
+                 hover = hoverOpts(
+                   id = "plot_hover",
+                   delay = 200,
+                   delayType = "throttle")
+      )
+    ),
+    box(
+      title = 'Debugging area',
+      width = 12,
+      verbatimTextOutput('debugging'),
+      collapsed = TRUE
+    )
   )
-)
+  #controlbar = dashboardControlbar()
+)}
 
+################ server function ########################
 server <- function(input, output, session) {
   
-  # TODO drawing_finished <- reactiveVal(FALSE)
-  
-  coords <- reactiveValues(x=NULL, y=NULL)
-  # draw() simply controls whether to collect hover coordinates or not
-  draw <- reactiveVal(FALSE)
-  
-  observeEvent(input$plot_click, handlerExpr = {
-    # hint: Click coordinates are needed in both drawing modes. Thus, logic is the same.
-    coords$x <- c(coords$x, input$plot_click$x)
-    coords$y <- c(coords$y, input$plot_click$y)
-    draw(!draw())
-    print(paste(draw(), input$drawType))
+  ######## Data related stuff #############
+  DataParams <- reactive({
+    group1_params <- list(
+      n1 = input$n1,
+      mean1 = input$mean1_1,
+      mean2 = if(input$equal_mean_1) input$mean1_1 else input$mean2_1,
+      sd1 = input$sd1_1,
+      sd2 = if(input$equal_sd_1) input$sd1_1 else input$sd2_1,
+      cor = input$cor_1)
+    
+    group2_params <- list(
+      n = input$n2,
+      mean1 = input$mean1_2,
+      mean2 = if(input$equal_mean_2) input$mean1_2 else input$mean2_2,
+      sd1 = input$sd1_2,
+      sd2 = if(input$equal_sd_2) input$sd1_2 else input$sd2_2,
+      cor = input$cor_2)
+    
+    list(group1_params = group1_params, group2_params = group2_params)
   })
   
-  observeEvent(c(input$drawType, input$reset), handlerExpr = {
-    coords$x <- NULL
-    coords$y <- NULL
-    draw(FALSE)
+  data <- reactive({
+    generateData(DataParams()$group1_params, DataParams()$group2_params)
+  })
+   
+  ################ drawing logic ############################
+  
+  # draw() controls whether to collect hover coordinates or not
+  draw <- reactiveVal(FALSE)
+  drawing_finished <- reactiveVal(FALSE)
+  coords <- reactiveValues(x=NULL, y=NULL)
+  
+  observeEvent(input$plot_click, handlerExpr = {
+    if (!drawing_finished()){
+      # hint: Clicking is needed in both drawing modes. Thus, logic is the same.
+      draw(!draw())
+      # indicate finished drawing
+      if (draw()){
+        # hint: We do not collect the click coordinates but the hover coordinates, as clicking slightly moves the mouse pointer which might lead to edges or bends at the esp. in case of line mode
+        coords$x <- c(coords$x, input$plot_hover$x)
+        coords$y <- c(coords$y, input$plot_hover$y)
+      } else {
+        # draw() is set FALSE by second click, thus drawing_finished() becomes TRUE (and triggers classification evaluation)
+        drawing_finished(TRUE)
+      }
+    }
+    info(logger, paste('draw(): ', draw(), ', ', input$drawType, 'finished: ', drawing_finished()))
   })
   
   observeEvent(input$plot_hover, {
-    if (draw()==TRUE){
+    if (draw() && !drawing_finished()){
       x <- input$plot_hover$x
       y <- input$plot_hover$y
       
@@ -64,36 +180,109 @@ server <- function(input, output, session) {
       line = {
         coords$x[2] <- x
         coords$y[2] <- y
-        #print(paste(input$drawType, coords$x, coords$y))
       }
       )
     }
   })
   
+  ###################### classification stuff ##############################
+  
+  ######### old stuff  ########
+  AboveData <- reactive({
+    abovetmp <- NULL
+    if (drawing_finished()){
+      info(logger, paste('calculating above below data for ', input$drawType))
+      switch(EXPR = input$drawType,
+             freehand = {
+               abovetmp <- is_above_freehandline(x = coords$x, y = coords$y, compare_data = data())
+             },
+             line = {
+               linear_params <- calculate_linear_boundary(input_coordinates = coords)
+               info(logger, linear_params)
+               abovetmp <- is_above_linear(intercept = linear_params$intercept, slope = linear_params$slope, compare_data = data())
+               info(logger, length(abovetmp))
+             }
+      )
+    }
+    return(abovetmp)
+  })
+  
+
+  # Render table
+  output$AboveTable <- renderTable({
+    req(AboveData())
+    # Count the occurrences of TRUE and FALSE
+    counts <- table(AboveData())
+    # Convert to data frame for rendering in table
+    counts_df <- data.frame(isAbove = names(counts), Count = as.numeric(counts))
+    return(counts_df)
+  })
+  
+  output$classificationTable <- renderTable({
+    req(AboveData())
+    # Calculate confusion matrix 
+    #print(AboveData())
+    #tmp <- calculate_classification_results(referenceData = data()$Group, aboveData = AboveData())
+    # summary_table <- classification_results() %>%
+    #   group_by(Group, classification) %>%
+    #   summarise(Count = n()) %>%
+    #   ungroup() %>%
+    #   pivot_wider(names_from = classification, values_from = Count, values_fill = 0)
+    # 
+    # # Modify column names
+    # colnames(summary_table) <- c('true group', paste0("pred_", unique(classification_results()$classification)))
+    
+    # return(summary_table)
+    return(NULL)
+  })
+  
+  
+  ###################### plotting stuff ####################################
+  base_plot <- reactive({
+    # hint: rbase plot does not allow for working layerwise as in ggplot, thus only for ggplot the base_plot is created
+    if (!input$use_rbaseplot) {
+      info(logger, 'creating base plot for ggplot...')
+      return(plotScatter_ggplot(inputdata = data(), xlab = "footlength cm", ylab = "weight kg"))
+    }
+  })
+  
+  plotboundary_width <- 2
+  plotboundary_color <- 'red'
+  
   output$plot <- renderPlot({
+    req(data())
+    # info(logger, 'plot rendering')
+    # test <- data() %>% colnames()
+    # info(logger, test)
     if (input$use_rbaseplot) {
       # Draw the initial plot
-      plot(mtcars$hp, mtcars$mpg, xlab = "Horsepower", ylab = "Miles per Gallon", lwd=2)
-      
+      plotScatter_rbase(inputdata = data(), xlab = "footlength cm", ylab = "weight kg")
       if (!is.null(coords$x)) {
-        lines(x=coords$x, y=coords$y, lwd=2)  
+        lines(x=coords$x, y=coords$y, lwd=plotboundary_width, col = plotboundary_color)
       }
     } else {
-      # Create ggplot object
-      p <- ggplot(data = mtcars, aes(x = hp, y = mpg)) +
-        geom_point() +
-        labs(x = "Horsepower", y = "Miles per Gallon") +
-        theme_bw()
-      
-      # Add lines based on the drawing mode
+      # ggplot version
+      p <- base_plot()
       if (!is.null(coords$x)) {
-        p <- p + geom_line(data = data.frame(x = coords$x, y = coords$y), aes(x = x, y = y), size = 2)
-      }
-      
-      # Print the plot
+        p <- base_plot() + geom_line(data = data.frame(x = coords$x, y = coords$y), aes(x = x, y = y), color = plotboundary_color, linewidth = plotboundary_width)
+      } 
       p
     }
   })
+  
+  ################ reset stuff ###########################
+  
+  observeEvent(c(input$drawType, input$reset), handlerExpr = {
+    info(logger, 'reset variables')
+    coords$x <- NULL
+    coords$y <- NULL
+    draw(FALSE)
+    drawing_finished(FALSE)
+    
+    # hint: use the <<- operator to assign NULL to the reactive expression outside its definition
+    #AboveData <<- NULL
+  })
+  
 }
 
 shinyApp(ui, server)
