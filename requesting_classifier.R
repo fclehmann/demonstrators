cf5650724a0efea2f8909c1fe996a789061221e2
Christoph Lehmann <christoph.lehmann@tu-dresden.de>
2024-11-19 18:27:24 +0100

    extended script for creating thumbnails and added thumbnails

--- requesting_classifier.R
+++ requesting_classifier.R
@@ -10,8 +10,10 @@ library(magrittr)
 url <- "http://127.0.0.1:8000/predict/"
 
 # List of images to predict
-setwd('~/Dokumente/Python_projects/understand_classification/traindata/vogel/')
+setwd('~/Dokumente/R_projects/demonstrators/classifier_shiny/data/thumbnails/')
 images <- list.files('~/Dokumente/Python_projects/understand_classification/traindata//', full.names = T, recursive = T)
+
+#### request image classification ####
 
 # Send the POST request with the list of image inputs
 response <- POST(
@@ -37,9 +39,58 @@ if (status_code(response) == 200) {
   print(paste("Request failed with status code:", status_code(response)))
 }
 
-
-
 animal_classifier_df %<>% mutate_all(unlist)# %>% as.data.frame()
-
-
 save(animal_classifier_df, file = '~/Dokumente/R_projects/demonstrators/classifier_shiny/data/animal_classifier-last_layer_data.Rdata')
+
+##### creating thumbnails of images ####
+# Load libraries
+library(imager)
+library(purrr)
+library(grid)
+
+process_images_sequential <- function(image_paths, target_dir, width = 50, height = 50) {
+  # Create a subdirectory named with the resolution (e.g., '50x50') inside the target directory
+  target_subdir <- file.path(target_dir, paste0(width, "x", height))
+  
+  # Create the subdirectory if it doesn't exist
+  if (!dir.exists(target_subdir)) {
+    dir.create(target_subdir, recursive = TRUE)
+  }
+  
+  # Apply the resize_image function to each image
+  resized_image_paths <- purrr::map(image_paths, ~ resize_image(.x, width, height, target_subdir))
+  
+  # Filter out NULL values (in case some files were skipped)
+  resized_image_paths <- Filter(Negate(is.null), resized_image_paths)
+  
+  return(resized_image_paths)
+}
+
+# Function to resize an image (modified for the new subdirectory)
+resize_image <- function(image_path, width = 50, height = 50, target_dir) {
+  if (file.exists(image_path)) {  # Only process if the file exists
+    cat("Processing:", image_path, "\n")  # Print the current image being processed
+    
+    img <- load.image(image_path)  # Load image using imager package
+    img_resized <- resize(img, size_x = width, size_y = height)
+    file_name <- basename(image_path)
+    target_path <- file.path(target_dir, paste0(file_name))
+    save.image(img_resized, target_path)
+    
+    return(target_path)
+  }
+  return(NULL)  # If the file doesn't exist, return NULL
+}
+
+# Define the target directory for resized images
+target_dir <- getwd()  # Target directory for resized images
+
+# Define the desired resolution
+width <- 30  # Desired width of the resized images
+height <- 30  # Desired height of the resized images
+
+# Call the function to process the images sequentially
+resized_image_paths <- process_images_sequential(image_paths, target_dir, width, height)
+
+
+
